e963bd38ac63b66ccdeea83ca1cc1b2e2375d4d8
Inoculate <55796442+Inoculate@users.noreply.github.com>
2022-06-01 23:10:28 +0100

    Alterações no menu e implementação de logo para o dark mode

--- app/src/main/java/com/example/travellorganizer/MainActivity.kt
+++ app/src/main/java/com/example/travellorganizer/MainActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.travellorganizer.databinding.ActivityMainBinding
 
@@ -26,10 +25,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_travell, R.id.navigation_list
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
