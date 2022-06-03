43345303adc5acdb38711a1e3ef805b9f6ec9a99
Tomás Esteves <tomsesteves@gmail.com>
2022-06-03 14:34:11 +0100

    Implementação das classes que criam as tabelas na base de dados

---
+++ app/src/main/java/com/example/travellorganizer/db/CategoriesTable.kt
@@ -0,0 +1,24 @@
+package com.example.travellorganizer.db
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+//Classe que cria a tabela Lista herda a class TableModel
+class CategoriesTable(db : SQLiteDatabase) : TableModel (db, NAME) {
+
+    //função que cria a tabela
+    override fun create(){
+        db.execSQL("CREATE TABLE $name " +
+                "(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$FIELD_NAME TEXT NOT NULL," +
+                "$FIELD_CATEGORY_ID INTEGER," +
+                "FOREIGN KEY($FIELD_CATEGORY_ID) REFERENCES $name(${BaseColumns._ID}) " +
+                ")")
+    }
+    //Nomes dos campos e da tabela
+    companion object {
+        const val NAME = "categories"
+        const val FIELD_NAME = "name"
+        const val FIELD_CATEGORY_ID = "category_id"
+    }
+}
---
+++ app/src/main/java/com/example/travellorganizer/db/ItemsTable.kt
@@ -0,0 +1,28 @@
+package com.example.travellorganizer.db
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+//Classe que cria a tabela Lista herda a class TableModel
+class ItemsTable(db : SQLiteDatabase) : TableModel (db, NAME){
+
+    //função que cria a tabela
+    override fun create(){
+        db.execSQL("CREATE TABLE $name " +
+                "(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$FIELD_NAME TEXT NOT NULL," +
+                "$FIELD_QUANTITY INTEGER," +
+                "$FIELD_CATEGORY_ID INTEGER," +
+                "FOREIGN KEY($FIELD_CATEGORY_ID) REFERENCES $TABLE_REFERENCE(${BaseColumns._ID}) " +
+                ")")
+    }
+
+    //Nomes dos campos e da tabela
+    companion object {
+        const val NAME = "items"
+        const val FIELD_NAME = "name"
+        const val FIELD_QUANTITY = "quantity"
+        const val FIELD_CATEGORY_ID = "category_id"
+        const val TABLE_REFERENCE = "categories"
+    }
+}
